e5a9ce8037ca533bd5107c4ab62e2dfbee9902e7
17Sergey <sergeykachaliukdev@gmail.com>
2024-12-10 14:31:07 +0300

    fix: delpoy

--- src/components/Header/HeaderMenu/styled.ts
+++ src/components/Header/HeaderMenu/styled.ts
@@ -1,8 +1,8 @@
 import { Link } from "react-router";
 import styled from "styled-components";
 
-import BurgerIcon from "@assets/icons/burgerMenu.svg";
-import CrossIcon from "@assets/icons/crossIcon.svg";
+import BurgerIcon from "@assets/icons/BurgerMenu.svg";
+import CrossIcon from "@assets/icons/CrossIcon.svg";
 import { device } from "@styles/breakpoints";
 
 export const StyledNav = styled.nav`
